178b735a6fc2b2240524ca69ae1db96b49aa3af0
Olganame <olka-1992@mail.ru>
2018-10-14 14:37:13 +0300

    Update task_1_1.py

--- task_1_1/task_1_1.py
+++ task_1_1/task_1_1.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-#12. Create a script called shortcut to remove all the lowercase vowels in a given string.
-
-#Examples:
-#shortcut("codewars") # --> cdwrs
-#shortcut("goodbye")  # --> gdby
 
 # In[37]:
 
